9b99cfbd1f2e65e7ef040a0a89276dc301c1663c
TotalChen <totalatot@gmail.com>
2018-03-01 14:53:19 +0800

    Update BoardConfigOmni.mk to build my TWRP

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -1,10 +1,3 @@
-# Inline kernel building
-TARGET_KERNEL_CONFIG := shamu_defconfig
-TARGET_KERNEL_SOURCE := kernel/moto/shamu
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
-
 # Include additional properties in separate system.prop
 TARGET_SYSTEM_PROP := device/moto/shamu/omni_system.prop
 
@@ -19,3 +12,17 @@ RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 TW_SCREEN_BLANK_ON_BOOT := true
 BOARD_SUPPRESS_SECURE_ERASE := true
+
+# Additions
+TARGET_USERIMAGES_USE_F2FS := true
+TW_DEFAULT_BRIGHTNESS := 30
+TW_NO_SCREEN_TIMEOUT := true
+#RECOVERY_SDCARD_ON_DATA := true
+#BOARD_HAS_NO_REAL_SDCARD := true
+#BOARD_HAS_NO_SELECT_BUTTON := true
+#BOARD_HAS_LARGE_FILESYSTEM := true
+
+# Asian region languages
+TW_EXTRA_LANGUAGES := true
+TW_DEFAULT_LANGUAGE := zh_CN
+
